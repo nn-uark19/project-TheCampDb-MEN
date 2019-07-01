466966d9ce16b7a91375d189422b33cc49e1f1a8
Nghia Nguyen <nhuunghia_1711@yahoo.com.vn>
2019-07-01 14:16:50 -0500

    indent correction, file name change, finish v2

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //==============================================
 // import library
 console.log('Begin app.js');
-const express = require('express');
-const mongoose = require('mongoose');
+const express   =   require('express'),
+      app       =   express(),
+      mongoose  =   require('mongoose'),      
+      bodyParser =  require('body-parser');
 mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
-const app = express();
-const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
@@ -50,49 +50,48 @@ app.get('/', function(req, res) {
 
 // INDEX route (/campgrounds GET): index get
 app.get('/campgrounds', function(req, res){
-  console.log('Route app.get(/campgrounds)');
+  console.log('Route_index app.get(/campgrounds)');
   Campground.find({}, function(err, allCamps){
     if (err) {
       console.log(err);
     } else {
-      console.log(' Get data from mongoose successfully, render to campgrounds');
-      res.render('index', {campgrounds: allCamps})
+      console.log(' mongoose find successfully, render to route_index.ejs');
+      res.render('route_index', {campgrounds: allCamps})
     }
   });
 })
 
 // NEW route (/campgrounds/new GET): to add campground, use post /campgrounds
 app.get('/campgrounds/new', function(req, res){
-  console.log('Route app.get(/campgrounds/new)');
-  res.render('addCamp');
+  console.log('Route_new app.get(/campgrounds/new)');
+  res.render('route_new');
 })
 
 // CREATE route (/campgrounds POST): to insert data, redirect to get /campgrounds GET
 app.post('/campgrounds', function(req, res){
-  console.log('Route app.post(/campgrounds)');
+  console.log('Route_create app.post(/campgrounds)');
   const newCamp = {name: req.body.name, image: req.body.image, description: req.body.description};
   Campground.create(newCamp, function(err, campAdd){
     if (err) {
       console.log(err);
     } else {
-      console.log(' Add data to mongoose successfully, redirect');
-      console.log(campAdd);
+      console.log(' mongoose.create successfully, redirect to /campgrounds');
+      res.redirect('/campgrounds');
     }
   });
-  res.redirect('/campgrounds');
 })
 
 // SHOW	route (/campgrounds/:id	GET)
 app.get('/campgrounds/:id',function(req, res){
-  console.log('Route app.get(/campgrounds/:id)');
+  console.log('Route_show app.get(/campgrounds/:id)');
   var idToBeFound = req.params.id;
   Campground.find({'_id':idToBeFound}, function(err, foundCamp){
     if (err) {
       console.log(err);
     } else {
-      console.log(' Found campground with id, render to campgrounds');
+      console.log(' mongoose.find success, render to route_show.ejs');
       console.log(foundCamp);
-      res.render("campgrounds", {camp: foundCamp});
+      res.render("route_show", {camp: foundCamp});
     }
   })
 });
